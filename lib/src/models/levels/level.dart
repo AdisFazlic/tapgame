4fc764371de99cde40fd4475074e504ba2b8b4aa
AdisFazlic <58154763+AdisFazlic@users.noreply.github.com>
2019-12-05 08:16:50 +1100

    Add files via upload

--- lib/src/models/levels/level.dart
+++ lib/src/models/levels/level.dart
@@ -1,14 +1,23 @@
 import 'package:boloids/src/models/target.dart';
+import 'dart:math';
 
 class Level {
-  List<Target> targets = [];
+  
+  // TODO: this List should contain all the Targets that appeard at this Level
+  // TODO: this is separate from the idea that multiple targets may appear at the same time in a level
+  List<Target> targets = []; // we may have multiple targets in the same level
   Duration duration = Duration(seconds: 15);
   int score = 0;
   String name = '';
 
+
   Level();
 
-  Target get singleTarget => targets[0];
+  Level.withDefaultTarget() {
+    targets.add(Target());
+  }
+
+  Target get firstTarget => targets[0]; // return the first target in the list
 
   void get resetScore => score = 0;
 }
